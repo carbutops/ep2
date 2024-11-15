2d6b0d0775e588aa399ce4c2d23d2ae52568f829
Pietro Luigi Baldan Carbone <carbutops@gmail.com>
2024-11-15 18:49:29 -0300

    adicionados traços de url basico

--- moviesite/ep2s/urls.py
+++ moviesite/ep2s/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import include, path # modificar esta linha
+from . import views
+
 
 urlpatterns = [
      # adicionar esta linha
-    path('admin/', admin.site.urls),
-    path('', include('ep2s.urls')),
+    path('', views.index, name= 'index'),
 ]
