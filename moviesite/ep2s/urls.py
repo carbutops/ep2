c3b7360e87b09987c6d1cdaae80f93288b4c0440
Pietro Luigi Baldan Carbone <carbutops@gmail.com>
2024-11-15 19:28:58 -0300

    adicionando topic especifico

--- moviesite/ep2s/urls.py
+++ moviesite/ep2s/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
      # adicionar esta linha
     path('', views.index, name= 'index'),
+    path('topics', views.topics, name= 'topics'),
+    path('topics/<topic_id>/', views.topic, name= 'topic'),
 ]
