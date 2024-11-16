7ce908621a3f37813f5b106d0bfa5854e835e09f
Pietro Luigi Baldan Carbone <carbutops@gmail.com>
2024-11-15 23:07:46 -0300

    por algum motivo n tinha as entrys

--- moviesite/ep2s/views.py
+++ moviesite/ep2s/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Topic, Entry
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 def index(request):
     return render(request, 'ep2s/index.html')
@@ -15,4 +18,16 @@ def topic(request,topic_id):
     entries= topic.entry_set.order_by('-date_added')
     context={ 'topic': topic, 'entries': entries}
     return render(request, 'ep2s/topic.html', context)
+
+def new_topic(request):
+    if request.method!= 'POST':
+        form= TopicForm()
+    else:
+        form= TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    context={'form': form}
+    return render( request, 'ep2s/new_topic.html', context)
+
 
